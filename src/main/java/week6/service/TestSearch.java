f773d2d6ce008f15a12f5fe40a5af1e9da1eef89
tkddn2356 <tkddn2356@gmail.com>
2020-05-27 20:59:38 +0900

    no message

--- src/main/java/week6/service/TestSearch.java
+++ src/main/java/week6/service/TestSearch.java
@@ -9,22 +9,19 @@ import java.util.List;
 @Service
 public class TestSearch implements Search {
     private List<Integer> list = new ArrayList<Integer>();
-    private int key = 999999900; // 찾으려는 키
+    private int key = 90000000;
 
-    // 객체가 생성될 때 랜덤한 수들을 리스트에 넣는다.
     public TestSearch() {
-        for (int i = 0; i < 1000000000; i++) {
+        for (int i = 0; i < 100000000; i++) {
             list.add(i);
         }
     }
-    // 순차 탐색을 수행한다.
     @Override
     public void SequentialSearch() {
         System.out.println("SequentialSearch");
         list.indexOf(key);
     }
 
-    // 정렬 ~ 이진탐색까지 시간측정에 넣었다.
     @Override
     public void BinarySearch() {
         System.out.println("BinarySearch");
